2b488ba9af8b51a4c6184da9e246e7bd0e34eb65
arun0580 <arun.raj@nfonics.com>
2025-08-25 14:49:42 +0530

    design update

--- src/screens/Community.jsx
+++ src/screens/Community.jsx
@@ -100,59 +100,61 @@ export const Community = () => {
   ];
 
   return (
-    <section className="relative w-full bg-white py-16 md:py-24 pb-0 md:pb-0 overflow-hidden">
-      {/* Hero Section */}
-      <div className="relative max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center px-4 md:px-12">
-        {/* Left Column */}
-        <div className="space-y-6 order-2 md:order-1 relative">
-          <h1 className="text-[40px] md:text-[64px] [font-family:'Anton',Helvetica] font-normal text-[#98bb3c] leading-tight">
-            Your Community.
-            <br />
-            Your Way.
-            <br />
-            All In One Place!
-          </h1>
+    <div className="relative w-full min-h-screen bg-white overflow-hidden">
+      <section className="py-16 md:py-24">
+        {/* Hero Section */}
+        <div className="relative max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center px-4 md:px-12">
+          {/* Left Column */}
+          <div className="space-y-6 order-2 md:order-1 relative">
+            <h1 className="text-[40px] md:text-[64px] [font-family:'Anton',Helvetica] font-normal text-[#98bb3c] leading-tight">
+              Your Community.
+              <br />
+              Your Way.
+              <br />
+              All In One Place!
+            </h1>
 
-          <p className="[font-family:'Jost',Helvetica] font-normal text-[#565656] text-lg md:text-2xl leading-normal">
-            "A single platform where curious minds can explore everything about
-            their community, connect with each other, support education, learn,
-            grow, and do together with just a click!"
-          </p>
+            <p className="[font-family:'Jost',Helvetica] font-normal text-[#565656] text-lg md:text-2xl leading-normal">
+              "A single platform where curious minds can explore everything
+              about their community, connect with each other, support education,
+              learn, grow, and do together with just a click!"
+            </p>
 
-          <div className="flex justify-start">
-            <div className="pt-4 cursor-pointer w-full md:w-[300px]">
-              <Button
-                property1="default"
-                className="w-full cursor-pointer"
-                text={<>COMING SOON!</>}
+            <div className="flex justify-start">
+              <div className="pt-4 cursor-pointer w-full md:w-[300px]">
+                <Button
+                  property1="default"
+                  className="w-full cursor-pointer"
+                  text={<>COMING SOON!</>}
+                />
+              </div>
+            </div>
+
+            {/* Decorative Image */}
+            <div className="absolute -top-6 right-0 w-40 md:w-96">
+              <img
+                className="w-full h-auto object-contain"
+                alt="Community illustration"
+                src="https://c.animaapp.com/zIBKH2hr/img/mask-group@2x.png"
               />
             </div>
           </div>
 
-          {/* Decorative Image */}
-          <div className="absolute -top-6 right-0 w-40 md:w-96">
+          {/* Right Column */}
+          <div className="relative flex justify-center items-center order-1 md:order-2">
             <img
-              className="w-full h-auto object-contain"
-              alt="Community illustration"
-              src="https://c.animaapp.com/zIBKH2hr/img/mask-group@2x.png"
+              className="w-[300px] md:w-[500px] object-contain"
+              alt="Community vector"
+              src="https://c.animaapp.com/zIBKH2hr/img/vector-1@2x.png"
+            />
+            <img
+              className="absolute top-0 right-0 w-[100px] md:w-[200px] object-contain"
+              alt="Community mask"
+              src="https://c.animaapp.com/zIBKH2hr/img/mask-group-1@2x.png"
             />
           </div>
         </div>
-
-        {/* Right Column */}
-        <div className="relative flex justify-center items-center order-1 md:order-2">
-          <img
-            className="w-[300px] md:w-[500px] object-contain"
-            alt="Community vector"
-            src="https://c.animaapp.com/zIBKH2hr/img/vector-1@2x.png"
-          />
-          <img
-            className="absolute top-0 right-0 w-[100px] md:w-[200px] object-contain"
-            alt="Community mask"
-            src="https://c.animaapp.com/zIBKH2hr/img/mask-group-1@2x.png"
-          />
-        </div>
-      </div>
+      </section>
       {/* Background full width */}
       <div className="relative w-full mt-10 md:mt-20">
         <img
@@ -194,7 +196,7 @@ export const Community = () => {
                     #{location.rank}
                   </div>
                 </div>
-                <p className="[font-family:'Fredoka',Helvetica] text-maindarkprimary text-sm text-center mt-2">
+                <p className="font-body-text-regular text-maindarkprimary text-center mt-2">
                   {location.name}
                 </p>
               </div>
@@ -386,6 +388,6 @@ export const Community = () => {
           </button>
         </div>
       </section>
-    </section>
+    </div>
   );
 };
